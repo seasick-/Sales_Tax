667cb574f35027bb1ba48dbf5429f543e2b9f7ef
= <=>
2014-03-29 22:33:04 -0700

    really working now

--- app/assets/js/browser.js
+++ app/assets/js/browser.js
@@ -7,24 +7,11 @@ $(function() {
 	var trains = new Trains();
 	var graph = trains.returnGraph();
 
-	var aNode = graph.getNode('A')
-	console.log(graph);
-	var aPropNames;
-	aPropNames = Object.getOwnPropertyNames(aNode._outEdges);
-
 	function getOutEdges(Node) {
 		var Node = graph.getNode(Node);
 		var NodePropNames;
 		NodePropNames = Object.getOwnPropertyNames(Node._outEdges);
 		return NodePropNames;
-	}
-
-	function makeArray(array_in){
-		var result=[];
-		for (var each in array_in){
-			result.push(array_in[each]);
-		}
-		return result;
 	}
 
 	var aEdges = getOutEdges('A');
@@ -42,7 +29,7 @@ $(function() {
 			for (var each2 in getOutEdges(aEdges[each][each1])){
 				for (var i=0; i<getOutEdges(getOutEdges(aEdges[each][each1])[each2]).length; i++){
 					if (getOutEdges(getOutEdges(aEdges[each][each1])[each2])[i]==='C'){
-						result[each]+= getOutEdges(getOutEdges(aEdges[each][each1])[each2])[i];
+						result[each]+= getOutEdges(aEdges[each][each1])[i]  ;
 					}					
 				}
 				console.log('  ',getOutEdges(getOutEdges(aEdges[each][each1])[each2]));
@@ -50,15 +37,17 @@ $(function() {
 		}
 	}
 
-for (var i=0; i<result.length; i++){
-	result[i]= 'A'+result[i];
-}
-console.log(result);
+	console.log(result);
+	for (var i=0; i<result.length; i++){
+		result[i]= 'A'+result[i];
+	}
+	console.log(result);
 
-for (var each in result){
-	var out = trains.routeLength(result[each])
-	console.log(out);
-}
+	var out=[];
+	for (var each in result){
+		out.push(trains.routeLength(result[each]));
+	}
+	console.log(Math.min.apply(Math,out));
 
 });
 
