ca73b960514b8cbd513a2ae78de827ba1e65890b
= <=>
2014-03-29 17:38:11 -0700

    working

--- server.js
+++ server.js
@@ -19,16 +19,8 @@ function startServer() {
   var http     = require('http');
   var path     = require('path');
   var port     = process.env.PORT || 3000;
-  var passport = require('passport');
   var flash    = require('connect-flash');
   var mongoose = require('mongoose');
-  require('./config/passport')(passport);
-
-
-  // set up consolidate and handlebars templates
-  app.engine('hbs', cons.handlebars);
-  app.set('view engine', 'hbs');
-  app.set('views', __dirname + '/app/assets/templates');
 
   app.configure(function() {
     app.use(express.json());
@@ -41,12 +33,6 @@ function startServer() {
     // session secret TODO move to node-foreman's .env / process.env
     var session_secret = process.env.OAA_SESSION_SECRET || 'CHANGEMECHANGEMECHANGEMECHANGEME';
     app.use(express.session({ store: new RedisStore(), secret: session_secret }));
-    app.use(passport.initialize());
-    // persistent login sessions (do not want for REST API)
-    app.use(passport.session());
-    // use connect-flash for flash messages stored in session
-    app.use(flash());
-    app.use(app.router);
   });
 
   app.configure('development', function() {
@@ -58,42 +44,6 @@ function startServer() {
   app.configure('test', function() {
     mongoose.connect('mongodb://localhost/oaa-test');
   });
-
-  // server generated pages routes
-  require('./app/routes')(app, passport);
-
-  // api routes
-  var users = require('./api/routes/userRoutes');
-  var meetings = require('./api/routes/meetingRoutes');
-  var agendaItems = require('./api/routes/agendaItemRoutes');
-
-  // Users routes
-  app.get('/api/v1/users', users.collection);
-  app.get('/api/v1/users/:id', users.findById);
-  app.post('/api/v1/users', users.create);
-  app.put('/api/v1/users/:id', users.update);
-  app.delete('/api/v1/users/:id', users.destroy);
-
-  // Meetings Routes
-  app.get('/api/v1/meetings', meetings.collection);
-  app.get('/api/v1/meetings/:id', meetings.findById);
-  app.post('/api/v1/meetings', meetings.create);
-  app.put('/api/v1/meetings/:id', meetings.update);
-  app.delete('/api/v1/meetings/:id', meetings.destroy);
-
-  // Agenda Item Routes
-  app.get('/api/v1/meetings/:meeting_id/agenda_items', agendaItems.collectionByMeeting);
-  app.get('/api/v1/agenda_items', agendaItems.collection);
-  app.get('/api/v1/agenda_items/:id', agendaItems.findById);
-  app.post('/api/v1/meetings/:meeting_id/agenda_items', agendaItems.createByMeeting);
-  app.post('/api/v1/agenda_items', agendaItems.create);
-  app.put('/api/v1/agenda_items/:id', agendaItems.update);
-  app.delete('/api/v1/agenda_items/:id', agendaItems.destroy);
-
-  // uncomment this if you want to use pushState:true in UserRouter.js start
-  // app.get('/users*', function(req, res) {
-  //   res.redirect('/#users' + req.params);
-  // });
 
   var server = http.createServer(app);
 
