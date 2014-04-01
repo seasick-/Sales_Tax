149434d0744fc977ea9c2a76e49c7594e58efa2e
= <=>
2014-03-31 18:54:10 -0700

    form finally working

--- app/assets/js/views/ItemCollectionView.js
+++ app/assets/js/views/ItemCollectionView.js
@@ -10,10 +10,18 @@ module.exports = Backbone.View.extend({
 	},
 
 	render: function() {
-		console.log(this.options.totalPrice, this.options.totalTax, this.options.totals);
-		console.log(this.collection.toJSON());
+		// console.log(this.options.totalPrice, this.options.totalTax, this.options.totals);
+		// console.log(this.collection.toJSON());
+
 		var itemCollectionsView1 = template1(this.collection.toJSON());
+		var itemCollectionsView2 = template2({
+			total:this.options.totals.total,
+			tax:this.options.totals.tax
+		});
+		console.log(itemCollectionsView2.el);
 		this.$el.html(itemCollectionsView1);
+		this.$('#CollectionView2').replaceWith(itemCollectionsView2);
+
 		$('#results').html(this.$el.html());
 		return this;  
 	}
